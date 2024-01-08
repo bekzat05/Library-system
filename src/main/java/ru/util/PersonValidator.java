2898f7ab92541e9267371e3acd2d37a178a515ec
bekzat.aitbekov <bekzat.aitbekov@nu.edu.kz>
2024-01-08 07:58:02 +0600

    first commit

---
+++ src/main/java/ru/util/PersonValidator.java
@@ -0,0 +1,34 @@
+package ru.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.dao.PersonDAO;
+import ru.models.Person;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Person.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        Person person = (Person) target;
+
+        if(personDAO.getPersonByName(person.getPersonName()).isPresent())
+            errors.rejectValue("personName", "", "Person with this name already exists!");
+    }
+}
